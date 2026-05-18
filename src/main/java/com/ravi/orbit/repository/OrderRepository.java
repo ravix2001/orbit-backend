ccc0456658b8e37ecea1af100fbe5f6e99a30725
ravix2001 <ravipandit2001.11.02@gmail.com>
2026-05-18 08:42:46 +0545

    getOrder

--- src/main/java/com/ravi/orbit/repository/OrderRepository.java
+++ src/main/java/com/ravi/orbit/repository/OrderRepository.java
@@ -3,6 +3,8 @@ package com.ravi.orbit.repository;
 import com.ravi.orbit.dto.OrderDTO;
 import com.ravi.orbit.entity.Order;
 import com.ravi.orbit.entity.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -15,13 +17,15 @@ public interface OrderRepository extends JpaRepository<Order, UUID> {
             " o.totalDiscount, o.totalSellingPrice, o.orderStatus, o.orderDate, o.deliveryDate ) " +
             " FROM Order o " +
             " WHERE o.customerId = :customerId ")
-    OrderDTO getOrdersByCustomerId(UUID customerId);
+    Page<OrderDTO> getOrdersByCustomerId(UUID customerId, Pageable pageable);
 
     @Query(" SELECT NEW com.ravi.orbit.dto.OrderDTO(o.id, o.orderNumber, o.totalItems, o.totalMarketPrice, " +
             " o.totalDiscount, o.totalSellingPrice, o.orderStatus, o.orderDate, o.deliveryDate ) " +
             " FROM Order o " +
             " WHERE o.sellerId = :sellerId ")
-    OrderDTO getOrdersBySellerId(UUID sellerId);
+    Page<OrderDTO> getOrdersBySellerId(UUID sellerId, Pageable pageable);
 
     List<Order> customer(User customer);
+
+    UUID seller(User seller);
 }
--- src/main/java/com/ravi/orbit/service/IOrderService.java
+++ src/main/java/com/ravi/orbit/service/IOrderService.java
@@ -1,12 +1,16 @@
 package com.ravi.orbit.service;
 
 import com.ravi.orbit.dto.OrderDTO;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.util.UUID;
 
 public interface IOrderService {
 
-    OrderDTO getOrdersByCustomerId(UUID userId);
+    Page<OrderDTO> getOrdersByCustomerId(UUID customerId, Pageable pageable);
+
+    Page<OrderDTO> getOrdersBySellerId(UUID sellerId, Pageable pageable);
 
 //    OrderDTO createOrder(OrderDTO request);
 
