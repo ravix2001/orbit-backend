5006f26e98b4c38dd6905b96f3378e8e7f2c3b8f
ravix2001 <ravipandit2001.11.02@gmail.com>
2026-05-31 08:12:28 +0545

    test removed

--- src/test/java/com/ravi/orbit/OrbitApplicationTests.java
+++ src/test/java/com/ravi/orbit/OrbitApplicationTests.java
@@ -1,13 +1,13 @@
-package com.ravi.orbit;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class OrbitApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package com.ravi.orbit;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class OrbitApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
