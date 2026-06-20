bf458ec403ae5e96057964d8364a059377d317bf
ravix2001 <ravipandit2001.11.02@gmail.com>
2026-06-20 08:35:24 +0545

    scheduler for expired tokens cleanup

--- src/main/java/com/ravi/orbit/repository/RefreshTokenRepository.java
+++ src/main/java/com/ravi/orbit/repository/RefreshTokenRepository.java
@@ -3,20 +3,15 @@ package com.ravi.orbit.repository;
 import com.ravi.orbit.entity.RefreshToken;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
 import java.util.Optional;
 import java.util.UUID;
 
 public interface RefreshTokenRepository extends JpaRepository<RefreshToken, UUID> {
 
-    Optional<RefreshToken> findByToken(String token);
-
     Optional<RefreshToken> findByTokenAndRevokedFalse(String token);
 
-    void deleteByToken(String token);
-
-    void deleteByUsername(String username);
-
-    void deleteAllByUsername(String username);
+    void deleteByExpiryDateBefore(LocalDateTime expiryDateBefore);
 
 }
 
--- src/main/java/com/ravi/orbit/scheduler/ScheduledTasks.java
+++ src/main/java/com/ravi/orbit/scheduler/ScheduledTasks.java
@@ -1,7 +1,45 @@
 package com.ravi.orbit.scheduler;
 
+import com.ravi.orbit.repository.RefreshTokenRepository;
+import com.ravi.orbit.service.IEmailService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDateTime;
+
+@Slf4j
 @Component
-public class UserScheduler {
+@Transactional
+@RequiredArgsConstructor
+public class ScheduledTasks {
+
+//    private final IEmailService emailService;
+
+    private final RefreshTokenRepository refreshTokenRepository;
+
+//    public void sendEmailVerification() {
+//
+//    }
+//
+//    public void sendPasswordResetEmail() {
+//
+//    }
+//
+//    public void sendEmailReminder() {
+//
+//    }
+//
+//    public void sendEmailNotification() {
+//
+//    }
+
+    @Scheduled(cron = "0 0 0 * * 1")
+    public void clearExpiredSessions() {
+        refreshTokenRepository.deleteByExpiryDateBefore(LocalDateTime.now());
+        log.info("Expired sessions cleared");
+    }
+
 }
